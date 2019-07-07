ac7d90ceff626e24f6d95971fef8cfdee2f4fd9e
Maryana Kravets <marjana_s@ukr.net>
2019-07-07 09:44:48 +0300

    Two files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,21 +2,6 @@ package main.java;
 
 import java.util.*;
 
-class MyComparator implements Comparator<Integer> {
-    public int compare(Integer t1, Integer t2) {
-        if (t1 % 2 == 0 && t2 % 2 != 0) {
-            return -1;
-        } else if (t1 % 2 != 0 && t2 % 2 == 0) {
-            return 1;
-        } else if (t1 % 2 == 0 && t2 % 2 == 0) {
-            return t1.compareTo(t2);
-        } else if (t1 % 2 != 0 && t2 % 2 != 0) {
-            return t1.compareTo(t2);
-        }
-        return 0;
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
         MyComparator myComparator = new MyComparator();
---
+++ src/main/java/MyComparator.java
@@ -0,0 +1,21 @@
+package main.java;
+
+import java.util.Comparator;
+
+public class MyComparator implements Comparator<Integer>
+
+    {
+        public int compare(Integer t1, Integer t2) {
+        if (t1 % 2 == 0 && t2 % 2 != 0) {
+            return -1;
+        } else if (t1 % 2 != 0 && t2 % 2 == 0) {
+            return 1;
+        } else if (t1 % 2 == 0 && t2 % 2 == 0) {
+            return t1.compareTo(t2);
+        } else if (t1 % 2 != 0 && t2 % 2 != 0) {
+            return t1.compareTo(t2);
+        }
+        return 0;
+    }
+    }
+
