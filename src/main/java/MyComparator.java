7548b363e6035fee836e10ff607076b7fe96aebc
Maryana Kravets <marjana_s@ukr.net>
2019-07-14 13:36:05 +0300

    added &&

--- src/main/java/MyComparator.java
+++ src/main/java/MyComparator.java
@@ -4,9 +4,9 @@ import java.util.Comparator;
 
 public class MyComparator implements Comparator<Integer> {
     public int compare(Integer t1, Integer t2) {
-        if (t1 % 2 == 0 & t2 % 2 != 0) {
+        if (t1 % 2 == 0 && t2 % 2 != 0) {
             return -1;
-        } else if (t1 % 2 != 0 & t2 % 2 == 0) {
+        } else if (t1 % 2 != 0 && t2 % 2 == 0) {
             return 1;
         } else
             return t1.compareTo(t2);
